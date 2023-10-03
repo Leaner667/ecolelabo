c87717831cbd5d98a850468046bbd8df1c0af8a3
Diego Munoz Calvo <91964641+Leaner667@users.noreply.github.com>
2023-10-03 20:50:38 +0200

    Python test

---
+++ Hangman Complet.py
@@ -0,0 +1,118 @@
+def print_hangman(incorrect_guesses):
+    stages = [
+        '''
+           --------
+           |      |
+           |      
+           |      
+           |     
+           -
+        ''',
+        '''
+           --------
+           |      |
+           |      O
+           |      
+           |      
+           -
+        ''',
+        '''
+           --------
+           |      |
+           |      O
+           |     \|
+           |      
+           -
+        ''',
+        '''
+           --------
+           |      |
+           |      O
+           |     \|/
+           |      
+           -
+        ''',
+        '''
+           --------
+           |      |
+           |      O
+           |     \|/
+           |      |
+           -
+        ''',
+        '''
+           --------
+           |      |
+           |      O
+           |     \|/
+           |      |
+           |       \\
+           -
+        ''',
+        '''
+           --------
+           |      |
+           |      O
+           |     \|/
+           |      |
+           |     / \\
+           -
+        '''
+    ]
+
+    if incorrect_guesses < len(stages):
+        print(stages[incorrect_guesses])
+    else:
+        print("You lost! The hangman is fully hanged!")
+
+# Rest of the code remains the same...
+
+
+# Rest of the code remains the same...
+
+
+def print_word(word, guessed_letters):
+    display_word = ''
+    for letter in word:
+        if letter in guessed_letters:
+            display_word += letter
+        else:
+            display_word += '_'
+    return display_word
+
+def main():
+    word_to_guess = "bastien"  # Replace with the word to guess.
+    max_attempts = 6
+    guessed_letters = []
+    incorrect_guesses = 0
+
+    print("Welcome to Hangman!")
+    while True:
+        print_hangman(incorrect_guesses)
+        print("Word: ", print_word(word_to_guess, guessed_letters))
+
+        if "_" not in print_word(word_to_guess, guessed_letters):
+            print("Congratulations! You won!")
+            break
+
+        if incorrect_guesses >= max_attempts:
+            print("You lost! The word was:", word_to_guess)
+            break
+
+        guess = input("Guess a letter: ").lower()
+
+        if len(guess) != 1 or not guess.isalpha():
+            print("Please enter a valid single letter.")
+            continue
+
+        if guess in guessed_letters:
+            print("You already guessed that letter.")
+            continue
+
+        guessed_letters.append(guess)
+
+        if guess not in word_to_guess:
+            incorrect_guesses += 1
+
+if __name__ == "__main__":
+    main()
---
+++ hangman premier.py
@@ -0,0 +1,15 @@
+def print_first_hangman_stage():
+    first_stage = '''
+       --------
+       |      
+       |      
+       |     
+       -
+    '''
+    print(first_stage)
+
+
+print_first_hangman_stage()
+
+
+input("Press Enter to exit...")
